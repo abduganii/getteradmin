44b4a02a70bfef8ec36cf2e8c6b8f56dde5725dc
Abdugani Abduqaxxorov <abduganiabdu2@gmail.com>
2023-04-11 16:27:47 +0500

    fixed

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,14 +11,14 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <ul className='navbar-list' >
-                <li className='navbar-item' onClick={() => setPage(`Главная`)}>  <NavLink activeClassName className='navbar-link' to={'/'}  >Главная   </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Все сайты')}>  <NavLink activeClassName className='navbar-link' to={'/webSites'} >Все сайты   </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Продукты')}>  <NavLink activeClassName className='navbar-link' to={'/products'} >Продукты   </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Портфолио')}>  <NavLink activeClassName className='navbar-link' to={'/portfolio'} >Портфолио  </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Статьи')}>  <NavLink activeClassName className='navbar-link' to={'/Articles'} >Статьи  </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Пользователи')}>  <NavLink activeClassName className='navbar-link' to={'/users'} >Пользователи   </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Заказы')}>  <NavLink activeClassName className='navbar-link' to={'/Order'} >Заказы   </NavLink> </li>
-                <li className='navbar-item' onClick={() => setPage('Категории')}>  <NavLink activeClassName className='navbar-link' to={'/categories'} >Категории   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage(`Главная`)}>  <NavLink activeClassName className='navbar-link' to={routes.HOME}  >Главная   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Все сайты')}>  <NavLink activeClassName className='navbar-link' to={routes.WEBSITE} >Все сайты   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Продукты')}>  <NavLink activeClassName className='navbar-link' to={routes.PRODUCTS} >Продукты   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Портфолио')}>  <NavLink activeClassName className='navbar-link' to={routes.PORTFOLIO} >Портфолио  </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Статьи')}>  <NavLink activeClassName className='navbar-link' to={routes.ARTICLES} >Статьи  </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Пользователи')}>  <NavLink activeClassName className='navbar-link' to={routes.USERS} >Пользователи   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Заказы')}>  <NavLink activeClassName className='navbar-link' to={routes.ORDER} >Заказы   </NavLink> </li>
+                <li className='navbar-item' onClick={() => setPage('Категории')}>  <NavLink activeClassName className='navbar-link' to={routes.CATEGORIES} >Категории   </NavLink> </li>
 
             </ul>
             <p className='navbar-settings'> <NavLink className='navbar-link' to={routes.SETTINGS}>Настройки</NavLink></p>
